54c26fd78424aafd9951bdea8bf6dce0dfb70148
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-06-06 05:13:25 -0700

    mandatory task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for asci_letter in range(97, 113):
+for asci_letter in range(97, 123):
     if chr(asci_letter) != 'q' and chr(asci_letter) != 'e':
         print("{}".format(chr(asci_letter)), end="")

f5258dc3ba26a91aac2ebf45d091ed98ac67a7c3
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-07-03 00:18:02 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    """A simple attempt to represent Square"""
+    def __init___(self, square):
+        """initialize attribute"""
+        self.square = square

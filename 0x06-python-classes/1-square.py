97ada1f6a289ae76a06f61edbf33f50cdfb4ee24
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-07-03 00:55:28 -0700

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-suare.py by AbdulTechX
+"""Define a square"""
+
+
+class Square:
+    """Represent Square"""
+
+    def __init__(self, size):
+        """initializing a new Square"""
+        self.__size = size

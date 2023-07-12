ab50dc17a61e53a25acc458598b2afc022aa58b7
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-07-11 18:37:08 -0700

    python I/O task

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# AbdulTechX
+# 2-append_write.py
+"""Write a function that appends a string """
+
+
+def append_write(filename="", text=""):
+    """ write a string to utf8 text file.
+
+    Args:
+        filename (str): the name of the file to write.
+        text (str): The text to write to the file.
+    Return:
+         The number of characters written:
+    """
+    with open(filename, 'a', encoding="UTF-8") as file:
+        return(file.write(text))

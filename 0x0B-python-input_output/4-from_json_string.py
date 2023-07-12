fff38c031b327b1b2e195dd1b431a459a3e6487e
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-07-12 13:42:08 -0700

    python I/O tasks

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# AbdulTechX
+# 4-from_json_string.py
+"""Write a function that returns an object
+(Python data structure) represented by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object representation of a JSON string."""
+    return json.loads(my_str)

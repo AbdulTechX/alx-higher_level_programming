6fe344b8e2719f152f937dd3d53e3679e444567d
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-08-16 21:55:39 -0700

    SGL

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- lists all records with a score >= 10 in the table 
+-- Results should display both the score and the name (in this order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;)

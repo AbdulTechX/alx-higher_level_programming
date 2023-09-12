7b37d59fd8c44680bbd530c068f7dcf8bf87bdf6
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-09-12 06:51:11 -0700

    Javascript Object

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myRec = '';
+      for (let j = 0; j < this.width; ++j) myRec += 'X';
+      console.log(myRec);
+    }
+  }
+};

18b0dae1b8320830f2d1af544e2018d34aa7159f
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-09-16 06:15:27 -0700

    tasks

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, passwd=password,
+                         db=database)
+    cur = db.cursor()
+    query = "SELECT * FROM `states` WHERE `name` LIKE %s ORDER BY `id` ASC"
+    cur.execute(query, (state_name, ))
+
+    query_row = cur.fetchall()
+
+    for row in query_row:
+        print(row)
+
+    cur.close()
+    db.close()

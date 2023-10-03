551741bb116cdffdbde8e24ff57d17e8595a579e
AbdulTechX <abdulazeezbuhari27@gmail.com>
2023-10-03 09:42:42 -0700

    scripting

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Define the POST data with email and subject variables
-post_data="email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD"
-curl -s -d "$post_data" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"

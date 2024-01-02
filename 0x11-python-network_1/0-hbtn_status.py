18cbce4c882c929ac1014dc4c432890b0b3208ef
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-01-01 17:38:07 -0800

    Urllib request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,16 +10,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type:", type(body))
         print("\t- content:", body)
-        print("\t- utf8 content:", decoded_body)                                                         ~                                        #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
-
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        body = response.read()
-        decoded_body = body.decode('utf-8')
-
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
         print("\t- utf8 content:", decoded_body)

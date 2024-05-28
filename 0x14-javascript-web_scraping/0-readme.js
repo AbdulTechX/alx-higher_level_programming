523b1f347ffac5d8f9c814db308bb8fa8aab1067
AbdulTechX <abdulazeezbuhari27@gmail.com>
2024-05-28 15:07:44 -0700

    web Scrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 
 // Read the file using the provided file path argument
 fs.readFile(process.argv[2], 'utf8', function (error, content) {
-    console.log(error || content);
+  console.log(error || content);
 });
